4f4b69ed610ff71ab751a0c1e9bbb1effcc0e7a0
Youness Dendane <dendaneys@gmail.com>
2020-10-30 17:37:32 -0400

    Make generator functionnal

--- generators/electric/index.js
+++ generators/electric/index.js
@@ -1,31 +1,52 @@
+'use strict';
+
 const Generator = require('yeoman-generator');
+const path = require('path');
 
-module.exports = class extends Generator {
-  constructor(args, opts) {
-    super(args, opts);
-  }
+module.exports = Generator.extend({
+  constructor: function () {
+   Generator.apply(this, arguments); 
 
-  async prompting() {
-    this.answers = await this.prompt([
-      {
-        type: 'confirm',
-        name: 'tension',
-        message: 'Would you like to use voltage?',
-        default: true,
-      },
-      {
-        type: 'input',
-        name: 'electric',
-        message: 'Name of you electric module',
-      }
-    ]);
-  }
+   this.sourceRoot(path.join(__dirname, 'templates/generic-app'));
+  },
 
-  writing() {
-    this.log('electrical writing to files');
-  }
+  initializing: {
+  },
 
-  wire() {
-    this.log('wiring');
+  prompting: {
+    async confirmPlease() {
+      this.answers = await this.prompt([
+        {
+          type: 'confirm',
+          name: 'tension',
+          message: 'Would you like to use voltage?',
+          default: true,
+        },
+        {
+          type: 'input',
+          name: 'electric',
+          message: 'Name of you electric module',
+        }
+      ]);
+    }
+  },
+
+  writing: {
+    doTheWriting() {
+      this.log('electrical writing to files');
+      this.destinationRoot('RootFolder');
+
+      this.fs.copyTpl(this.templatePath('public/index.html'),
+        this.destinationPath('public/index.html'),
+        {
+          AppTitle: 'hello',
+        });
+    }
+  },
+
+  default: {
+    wire() {
+      this.log('wiring');
+    }
   }
-};
+});
--- generators/turbo/index.js
+++ generators/turbo/index.js
@@ -3,21 +3,46 @@ const Generator = require('yeoman-generator');
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
+
+    this.option('projectLanguage', { type: String, description: 'language for the project: js or ts' });
   }
 
   async prompting() {
+    this.log(`You choose ${this.options.projectLanguage}`);
+
     this.answers = await this.prompt([
       {
         type: 'confirm',
         name: 'turbo',
         message: 'Would you like to enable turbo?',
         default: true
+      },
+      {
+        when: (answers) => answers.turbo,
+        type: 'list',
+        name: 'model',
+        message: 'What type of turbo do you want?',
+        choices: ['V12', 'V8', 'V6', 'None'],
+        default: 'None'
       }
     ]);
   }
 
   writing() {
     this.log('turbo is now writing to files');
+
+    const pkgJson = {
+      devDependencies: {
+        eslint: '^3.15.0'
+      },
+      dependencies: {
+        react: '^16.2.0'
+      }
+    };
+  }
+
+  install() {
+    // this.npmInstall();
   }
 
   turbo() {

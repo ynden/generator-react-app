fb33f455ccf3c15511cf816198a78e64d17dac76
Youness Dendane <dendaneys@gmail.com>
2020-11-01 09:09:58 -0500

    Remove _git folder

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![publish](https://github.com/ynden/generator-react-app/workflows/Publish%20npm%20package/badge.svg?branch=master)](https://github.com/ynden/generator-react-app/workflows/Publish%20npm%20package/badge.svg?branch=master)
 
 [![npm version](https://badge.fury.io/js/generator-build-for-me.svg)](https://badge.fury.io/js/generator-build-for-me)
+
+[![GitHub version](https://badge.fury.io/gh/ynden%2FReact-Test.svg)](https://badge.fury.io/gh/ynden%2FReact-Test)
--- generators/electric/index.js
+++ generators/electric/index.js
@@ -5,9 +5,9 @@ const path = require('path');
 
 module.exports = Generator.extend({
   constructor: function () {
-   Generator.apply(this, arguments); 
+    Generator.apply(this, arguments);
 
-   this.sourceRoot(path.join(__dirname, 'templates/generic-app'));
+    this.sourceRoot(path.join(__dirname, 'templates/generic-app'));
   },
 
   initializing: {
@@ -26,6 +26,13 @@ module.exports = Generator.extend({
           type: 'input',
           name: 'electric',
           message: 'Name of you electric module',
+        },
+        {
+          when: answers => answers.tension,
+          type: 'input',
+          name: 'body',
+          message: 'Body message',
+          default: 'Welcome everybody!'
         }
       ]);
     }
@@ -39,9 +46,25 @@ module.exports = Generator.extend({
       this.fs.copyTpl(this.templatePath('public/index.html'),
         this.destinationPath('public/index.html'),
         {
-          AppTitle: 'hello',
+          AppTitle: this.answers.electric,
         });
+
+      this.fs.copy(`${this.templatePath('src/')}`, this.destinationPath('src/'));
+      this.fs.copyTpl(this.templatePath('src/App.js'), this.destinationPath('src/App.js'),
+        {
+          AppBody: this.answers.body
+        });
+
+      this.fs.copy(this.templatePath('package.json'), this.destinationPath('package.json'));
+      this.fs.copy(this.templatePath('README.md'), this.destinationPath('README.md'));
+
+      // Initialize git repository
+      this.spawnCommandSync('git', ['init', this.destinationPath()]);
     }
+  },
+
+  installing() {
+    this.yarnInstall();
   },
 
   default: {
